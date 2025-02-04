f0996bb813f073292e77d295741abfed2a69d17a
maimaimai-bit <hmaivu02@gmail.com>
2025-02-04 00:35:06 -0600

    commit

--- app.rb
+++ app.rb
@@ -2,8 +2,19 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:dice_form)
+end
+
+get("/process_roll") do
+  # Get the number of dice and sides from query params
+  @dice = params.fetch("dice").to_i
+  @sides = params.fetch("sides").to_i
+  
+  # Generate random rolls
+  @rolls = []
+  @dice.times do
+    @rolls.push(rand(1..@sides))
+  end
+
+  erb(:roll_result)
 end
